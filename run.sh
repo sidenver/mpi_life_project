85c0d6a9ede555f84eaaf898bf80143fdc5beb92
sidenver <hanchinshing@gmail.com>
2017-02-26 05:59:13 -0500

    try 32 process

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#SBATCH --ntasks=4
-#SBATCH -t 00:00:10
+#SBATCH --ntasks=32
+#SBATCH -t 00:00:30
 #SBATCH --mem-per-cpu=2048
 #SBATCH --exclusive
 
-module unload intel
-module load openmpi/gnu
 
 mpirun life_mpi ./life.data.1 100 250 250
