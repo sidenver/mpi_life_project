ca25d8db2c02cd7bf775629fc213e40394762cc8
sidenver <hanchinshing@gmail.com>
2017-02-25 22:10:47 -0500

    run ping pong

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --ntasks=40
-#SBATCH -t 00:01:00
+#SBATCH --ntasks=2
+#SBATCH -t 00:00:10
 #SBATCH --mem-per-cpu=2048
 #SBATCH --exclusive
 
 module unload intel
 module load openmpi/gnu
 
-mpirun mpi_hello
+mpirun ping_pong
