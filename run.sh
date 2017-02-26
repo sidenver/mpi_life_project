8ffdf2402df513418f6a2aa2bcbdf954cdeaa0ab
sidenver <hanchinshing@gmail.com>
2017-02-26 07:59:17 -0500

    run the whole thing!

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --ntasks=4
+#SBATCH --ntasks=32
 #SBATCH -t 00:00:30
 #SBATCH --mem-per-cpu=2048
 #SBATCH --exclusive
 
 
-mpirun life_mpi ./life.data.2 100 250 250
+mpirun life_mpi ./final.data 500 500 500
